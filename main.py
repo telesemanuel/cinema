185ac776a5d85aa775e5bea025c3bbe6fbb79257
teleszinho <emanuelteles06@gmail.com>
2024-08-03 12:41:09 -0300

    versão 2.0

--- main.py
+++ main.py
@@ -1,59 +1,64 @@
-#Importação de biblioteca
+# Importação de biblioteca
 import os
 from datetime import date
+import time
 
-#entrada de daos
-nome = input('Informe seu nome: ')
-idade = int(input('informe sua idade: '))
+# Entrada de daos
+nome = input("Informe um nome: ")
+idade = int(input("Informe sua idade: "))
 data = f"{date.today().day}/{date.today().month}/{date.today().year}"
 
-#limpa console
-os.system('cls')
+# Limpa console
+os.system("cls")
 
-#Inicio do loop
+# Inicio do loop
 while True:
-    print(f'{'-'*30} CINE PYTHON {'-'*30}\n')
-    print(f'{date}\n')
-    print('Sala 1 - A volta do parafuso que não foi - 12 anos') 
-    print('Sala 2 - A volta do parafuso que quase foi - Livre') 
-    print('Sala 3 - A volta do parafuso que não quis ir - 18 anos') 
-    print('Sala 4 - A volta do parafuso que foi - 16 anos') 
-    print('Sala 5 - A volta do parafuso que foi e não voltou - 18 anos') 
+    print(f"{"*"*30} Lista de Filmes {"*"*30}")
+    print(f"   Dia: {data}\n")
+    print("Sala 1 - O Poderoso Chefão - 14 anos") 
+    print("Sala 2 - Um Sonho de Liberdade - 16 anos") 
+    print("Sala 3 - A Lista de Schindler - 16 anos") 
+    print("Sala 4 - Forrest Gump - O Contador de Histórias - 14 anos") 
+    print("Sala 5 - O Rei Leão - Livre") 
 
-    #Escolha do usuário
-    sala = input('\nSala desejada: ')
+    # Escolha do usuário
+    sala = input("\nSala desejada: ")
 
-    #limpa console
-    os.system('cls')
+    # Limpa console
+    os.system("cls")
 
-    #verifica a sala escolhida
+    # Verifica a sala escolhida
     match sala:
-        case '1':
-            idade_minima = 12
-            filme = 'A volta do parafuso que não foi'
-        case '2': 
-            idade_minima = 0
-            filme = 'A volta do parafuso que quase foi'
-        case '3': 
-            idade_minima = 18
-            filme = 'A volta do parafuso que não quis ir'
-        case '4': 
+        case "1":
+            idade_minima = 14
+            filme = "O Poderoso Chefão"
+        case "2": 
             idade_minima = 16
-            filme = 'A volta do parafuso que foi'
-        case '5': 
-            idade_minima = 18
-            filme = 'A volta do parafuso que foi e não voltou'
+            filme = "Um Sonho de Liberdade"
+        case "3": 
+            idade_minima = 16
+            filme = "A Lista de Schindler"
+        case "4": 
+            idade_minima = 14
+            filme = "Forrest Gump - O Contador de Histórias"
+        case "5": 
+            idade_minima = 0
+            filme = "O Rei Leão"
         case _:
             print('Sala inexistente, favor selecionar outra opção.')
             continue
-    #Verificação da idade
+
+    # Verificação da idade
     if idade >= idade_minima:
-        print(f'Ingresso impresso no dia: {data}.\n')
-        print(f'Sala: {sala}.')
-        print(f'Usuário: {nome}.')
-        print('\nSucesso! Tenha um ótimo filme.')
+        print(f"Ingresso impresso no dia: {data}.\n")
+        print(f"Sala: {sala}.")
+        print(f"Usuário: {nome}.")
+        os.system("cls")
+        print("\nCompra confirmada! Aguarde a impressão do ingresso.")
+        time.sleep(3)
+        print("Retire o Ingresso. Tenha um Ótimo Filme!")
         break
     else:
-        print(f'{nome} Não possui a idade minima para o filme {filme}')
-        print('Favor selecionar outro filme!')
+        print(f"Você não possui a idade minima para o filme {filme}")
+        print("Favor selecionar outro filme!")
         continue
